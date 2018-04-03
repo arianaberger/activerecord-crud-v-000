d65f999ae15216c54afc5a0bd9ee32e65e6b2af5
Ariana Berger <ariana.berger@gmail.com>
2018-04-03 16:21:58 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -57,7 +57,8 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  __
+  movie = Movie.find(title: "Title", release_date: 2000, director: "Me")
+  movie.title
 end
 
 def can_find_using_where_clause_and_be_sorted
